fd9940d7b0fc4bcf722998c5ca32f2ddb67731cf
Juboy <aderibolejuwon@gmail.com>
2019-09-24 17:37:54 +0100

    First commit

---
+++ src/main/java/com/brains/websocsBE/WebsocsBeApplication.java
@@ -0,0 +1,13 @@
+package com.brains.websocsBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocsBeApplication.class, args);
+	}
+
+}
